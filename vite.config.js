541c5d642d7fd5fba7fa4e23499c235cb83ae9d6
panduridhofani-spec <panduridhofani@gmail.com>
2026-08-28 21:53:55 +0700

    Ubah web menjadi PWA agar bisa diinstal di Android

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,32 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Dihe Mart',
+        short_name: 'DiheMart',
+        description: 'Aplikasi Rekap Transaksi Dihe Mart',
+        theme_color: '#4f46e5',
+        background_color: '#ffffff',
+        display: 'standalone',
+        icons: [
+          {
+            src: '/favicon.jpg',
+            sizes: '192x192',
+            type: 'image/jpeg'
+          },
+          {
+            src: '/favicon.jpg',
+            sizes: '512x512',
+            type: 'image/jpeg'
+          }
+        ]
+      }
+    })
+  ],
 })
